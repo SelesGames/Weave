3cb080c2904dba8839e3aedc0b94919d71882c1d
Arash Emami <aemami99@gmail.com>
2012-11-17 11:53:43 -0800

    more

---
+++ WinRT/SelesGames.Common/Extension Methods/WebResponseExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Text;
+
+namespace System.Net
+{
+    public static class WebResponseExtensionMethods
+    {
+        #region Helper functions converting WebResponse and Stream to a string value
+
+        public static string GetResponseStreamAsString(this WebResponse response)
+        {
+            string result = null;
+            using (var stream = response.GetResponseStream())
+            {
+                result = stream.GetReadStream();
+            }
+            return result;
+        }
+
+        public static string GetResponseStreamAsString(this WebResponse response, Encoding encoding)
+        {
+            string result = null;
+            using (var stream = response.GetResponseStream())
+            {
+                result = stream.GetReadStream(encoding);
+            }
+            return result;
+        }
+
+        #endregion    
+    }
+}
