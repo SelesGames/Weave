33f5658a71f9c6159ceb09e86b0ab859e468ac5d
Arash Emami <aemami99@gmail.com>
2012-11-17 14:10:04 -0800

    cleanup WeaveRssServerProxy.cs

--- Windows Phone 8/weave.RSS/WeaveRssServerProxy.cs
+++ Windows Phone 8/weave.RSS/WeaveRssServerProxy.cs
@@ -31,110 +31,32 @@ namespace Weave.RSS
 
         List<FeedRequest> outgoingFeedRequests;
         HttpWebRequest request;
-        //IObserver<IEnumerable<FeedResult>> observer;
         static readonly TimeSpan pingTimeout = TimeSpan.FromSeconds(4);
-
-        //public static IObservable<IEnumerable<FeedResult>> GetFeedResultsAsync(List<FeedRequest> outgoingFeedRequests)
-        //{
-        //    return Observable.Create<IEnumerable<FeedResult>>(observer =>
-        //    {
-        //        var request = new WeaveRssServerProxy(outgoingFeedRequests, observer);
-        //        return request;
-        //    });
-        //}
-
-
-
-
-        #region Ping the server to see if it is up and running (and responsive)
-
-        //public static IObservable<bool> PingServerAsync()
-        //{
-        //    return Observable.Create<bool>(observer =>
-        //    {
-        //        var disp = Disposable.Empty;
-        //        try
-        //        {
-        //            disp = PING_URL.ToUri().ToWebRequest().GetWebResponseAsyncWithTimeout(pingTimeout).Subscribe(
-        //                o =>
-        //                {
-        //                    try
-        //                    {
-        //                        var response = o as HttpWebResponse;
-        //                        if (response.StatusCode == HttpStatusCode.OK)
-        //                        {
-        //                            observer.OnNext(true);
-        //                            observer.OnCompleted();
-        //                        }
-        //                        else
-        //                        {
-        //                            observer.OnNext(false);
-        //                            observer.OnCompleted();
-        //                        }
-        //                    }
-        //                    catch (Exception)
-        //                    {
-        //                        observer.OnNext(false);
-        //                        observer.OnCompleted();
-        //                    }
-        //                },
-        //                exception =>
-        //                {
-        //                    observer.OnNext(false);
-        //                    observer.OnCompleted();
-        //                });
-        //        }
-        //        catch(Exception)
-        //        {
-        //            observer.OnNext(false);
-        //            observer.OnCompleted();
-        //        }
-        //        return disp;
-        //    });
-        //}
-
-        #endregion
-
-
-
 
         public WeaveRssServerProxy(
             List<FeedRequest> outgoingFeedRequests)
-            //IObserver<IEnumerable<FeedResult>> observer)
         {
             this.outgoingFeedRequests = outgoingFeedRequests;
-            //this.observer = observer;
-
-            //GetLatestNews();
         }
 
         public async Task<List<FeedResult>> GetFeedResultsAsync()
         {
-            //try
-            //{
-                request = HttpWebRequest.CreateHttp(SERVICE_URL);
-                request.Method = "POST";
-                request.ContentType = "application/json";
+            request = HttpWebRequest.CreateHttp(SERVICE_URL);
+            request.Method = "POST";
+            request.ContentType = "application/json";
 
-                var writeSerializer = new DataContractJsonSerializer(typeof(List<FeedRequest>));
+            var writeSerializer = new DataContractJsonSerializer(typeof(List<FeedRequest>));
 
-                using (var requestStream = await request.GetRequestStreamAsync().ConfigureAwait(false))
-                {
-                    writeSerializer.WriteObject(requestStream, outgoingFeedRequests);
-                    requestStream.Close();
-                }
+            using (var requestStream = await request.GetRequestStreamAsync().ConfigureAwait(false))
+            {
+                writeSerializer.WriteObject(requestStream, outgoingFeedRequests);
+                requestStream.Close();
+            }
 
-                var response = await request.GetResponseAsync().ConfigureAwait(false);
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
 
-                var results = ParseFeedResultsFromWebResponse(response);
-                return results;
-                //observer.OnNext(results);
-                //observer.OnCompleted();
-            //}
-            //catch (Exception exception)
-            //{
-            //    observer.OnError(exception);
-            //}
+            var results = ParseFeedResultsFromWebResponse(response);
+            return results;
         }
 
 
