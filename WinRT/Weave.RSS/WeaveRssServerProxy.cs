08f0fc439667f4037fe3aff094c85ae9b4164595
Arash Emami <aemami99@gmail.com>
2012-11-17 14:10:30 -0800

    winrt

---
+++ WinRT/Weave.RSS/WeaveRssServerProxy.cs
@@ -0,0 +1,91 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Weave.RSS.DTOs.Incoming;
+using Weave.RSS.DTOs.Outgoing;
+
+namespace Weave.RSS
+{
+    internal class WeaveRssServerProxy : IDisposable
+    {
+        /// LOCAL
+        /// ****************************
+        //const string SERVICE_URL = "http://localhost:8086/Weave";
+        //const string PING_URL = "http://localhost:8086/Ping/";
+
+
+        /// STAGING
+        /// ****************************
+        //const string SERVICE_URL = "http://5c6ea5aab3a74d64ad1ba9bdb3718f13.cloudapp.net:8086/Weave";
+        //const string PING_URL = "http://5c6ea5aab3a74d64ad1ba9bdb3718f13.cloudapp.net:8086/Ping/";
+
+
+        /// PRODUCTION
+        /// ****************************
+        const string SERVICE_URL = "http://weave.cloudapp.net:8086/Weave?fsd=true";
+        const string PING_URL = "http://weave.cloudapp.net:8086/Ping/";
+
+        List<FeedRequest> outgoingFeedRequests;
+        HttpWebRequest request;
+        static readonly TimeSpan pingTimeout = TimeSpan.FromSeconds(4);
+
+        public WeaveRssServerProxy(
+            List<FeedRequest> outgoingFeedRequests)
+        {
+            this.outgoingFeedRequests = outgoingFeedRequests;
+        }
+
+        public async Task<List<FeedResult>> GetFeedResultsAsync()
+        {
+            request = HttpWebRequest.CreateHttp(SERVICE_URL);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            var writeSerializer = new DataContractJsonSerializer(typeof(List<FeedRequest>));
+
+            using (var requestStream = await request.GetRequestStreamAsync().ConfigureAwait(false))
+            {
+                writeSerializer.WriteObject(requestStream, outgoingFeedRequests);
+            }
+
+            var response = await request.GetResponseAsync().ConfigureAwait(false);
+
+            var results = ParseFeedResultsFromWebResponse(response);
+            return results;
+        }
+
+
+
+
+        #region Helper Methods
+
+        List<FeedResult> ParseFeedResultsFromWebResponse(WebResponse response)
+        {
+            using (var stream = response.GetResponseStream())
+            using (var gzip = new GZipStream(stream, CompressionMode.Decompress, false))
+            {
+                var feedResults = Serializer.Deserialize<List<FeedResult>>(gzip);
+                return feedResults;
+            }
+        }
+
+        #endregion
+
+
+
+
+        public void Dispose()
+        {
+            try
+            {
+                if (request != null)
+                    request.Abort();
+            }
+            catch (Exception) {}
+        }
+    }
+}
