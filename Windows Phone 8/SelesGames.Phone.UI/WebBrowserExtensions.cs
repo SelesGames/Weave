e2ab9bef39b92ce4f8a22f95154fe24cedf09518
Arash Emami <aemami99@gmail.com>
2012-12-03 12:35:18 -0800

    more changes in an attempt

--- Windows Phone 8/SelesGames.Phone.UI/WebBrowserExtensions.cs
+++ Windows Phone 8/SelesGames.Phone.UI/WebBrowserExtensions.cs
@@ -49,4 +49,46 @@ namespace Microsoft.Phone.Controls
             return navigated;
         }
     }
+
+    internal static class IObservableExtensions
+    {
+        public static Task<T> ToTask<T>(this IObservable<T> observable)
+        {
+            var hasValue = false;
+            var lastValue = default(T);
+            var disposable = new System.Reactive.Disposables.SingleAssignmentDisposable();
+
+            var tcs = new TaskCompletionSource<T>();
+
+            try
+            {
+                disposable.Disposable = observable.Subscribe(
+                    value =>
+                    {
+                        hasValue = true;
+                        lastValue = value;
+                    },
+                    ex =>
+                    {
+                        tcs.TrySetException(ex);
+                        disposable.Dispose();
+                    },
+                    () =>
+                    {
+                        if (hasValue)
+                            tcs.TrySetResult(lastValue);
+                        else
+                            tcs.TrySetException(
+                                new InvalidOperationException("no elements in this observable sequence - IObservableExtensions.ToTask"));
+                        disposable.Dispose();
+                    });
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+
+            return tcs.Task;
+        }
+    }
 }
