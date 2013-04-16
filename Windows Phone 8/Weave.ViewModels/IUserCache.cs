eb06b18da6e2903bc61d59dfc96d1d392f3483a4
Arash Emami <aemami99@gmail.com>
2013-04-16 06:25:45 -0700

    added bunch of portable projects

---
+++ Windows Phone 8/Weave.ViewModels/IUserCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weave.ViewModels
+{
+    public interface IUserCache
+    {
+        //Task<UserInfo> AddUserAndReturnUserInfo(UserInfo incomingUser);
+        //Task<UserInfo> GetUserInfo(Guid userId, bool refresh = false);
+        Task<IList<NewsItem>> GetNews(string category, bool refresh = false, int skip = 0, int take = 10);
+        Task<IList<NewsItem>> GetNews(Guid feedId, bool refresh = false, int skip = 0, int take = 10);
+
+        Task AddFeed(Feed feed);
+        Task RemoveFeed(Feed feed);
+        Task UpdateFeed(Feed feed);
+
+        Task MarkArticleRead(NewsItem newsItem);
+        Task MarkArticleUnread(NewsItem newsItem);
+    }
+}
---
+++ Windows Phone 8/Weave/ViewModels/NewsCollectionViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Weave.ViewModels
+{
+    public abstract class BaseNewsCollectionViewModel
+    {
+        List<Guid> feedIds;
+        protected IUserCache serviceClient;
+        protected Guid userId;
+
+        public ObservableCollection<NewsItem> News { get; private set; }
+        public int NewArticleCount { get; set; }
+        public int UnreadCount { get; set; }
+        public int TotalArticleCount { get; set; }
+
+        public BaseNewsCollectionViewModel()
+        {
+            this.News = new ObservableCollection<NewsItem>();
+        }
+
+        public abstract Task RefreshNews();
+    }
+
+    public class NewsCollectionCategoryViewModel : BaseNewsCollectionViewModel
+    {
+        string category;
+
+        public NewsCollectionCategoryViewModel(string category) 
+        {
+            this.category = category;
+        }
+
+        public override async Task RefreshNews()
+        {
+            var news = await serviceClient.GetNews(category, true);
+            base.News.OrderedDescendingUniqueInsert(news, o => o.LocalDateTime);
+        }
+    }
+
+    public class NewsCollectionFeedViewModel : BaseNewsCollectionViewModel
+    {
+        Guid feedId;
+
+        public NewsCollectionFeedViewModel(Guid feedId) 
+        {
+            this.feedId = feedId;
+        }
+
+        public override async Task RefreshNews()
+        {
+            var news = await serviceClient.GetNews(feedId, true);
+            base.News.OrderedDescendingUniqueInsert(news, o => o.LocalDateTime);
+        }
+    }
+}
