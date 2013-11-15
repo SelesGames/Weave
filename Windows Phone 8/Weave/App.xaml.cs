ef662be053e6d1286f008c71dcc79a4d5ec96b4a
Arash Emami <aemami99@gmail.com>
2013-11-15 13:34:50 -0800

    increased version number

--- Windows Phone 8/Weave/App.xaml.cs
+++ Windows Phone 8/Weave/App.xaml.cs
@@ -32,7 +32,7 @@ namespace weave
             var settings = new AppSettings
             {
                 AppName = "Weave",
-                VersionNumber = "8.5.6",
+                VersionNumber = "8.5.7",
                 IsTrial = false,//new Microsoft.Phone.Marketplace.LicenseInformation().IsTrial(),
                 AssemblyName = "weave",
                 LogExceptions = true,
