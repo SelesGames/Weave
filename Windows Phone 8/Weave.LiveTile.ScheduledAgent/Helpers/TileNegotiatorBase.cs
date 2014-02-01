cb9474271735b4e34271a7ceef401e70863fb3ea
Arash Emami <aemami99@gmail.com>
2014-02-01 13:31:26 -0800

    deleted unused code

--- Windows Phone 8/Weave.LiveTile.ScheduledAgent/Helpers/TileNegotiatorBase.cs
+++ Windows Phone 8/Weave.LiveTile.ScheduledAgent/Helpers/TileNegotiatorBase.cs
@@ -16,28 +16,6 @@ namespace Weave.LiveTile.ScheduledAgent
             this.tile = tile;
         }
 
-        //protected async Task<ImageSource> GetImageAsync(string url)
-        //{
-        //    var bitmap = new WriteableBitmap(0, 0);
-
-        //    var request = CreateResizerRequest(url);
-        //    request.AllowReadStreamBuffering = true;
-        //    var response = await request.GetResponseAsync();
-        //    using (var stream = response.GetResponseStream())
-        //    {
-        //        bitmap.SetSource(stream);
-        //        stream.Close();
-        //    }
-        //    return bitmap;
-        //}
-
-        //HttpWebRequest CreateResizerRequest(string imageUrl)
-        //{
-        //    var url = string.Format("http://sg-imaging.cloudapp.net/api/ImageResizer?quality=50&size=200&imageUrl={0}", HttpUtility.UrlEncode(imageUrl));
-        //    var request = HttpWebRequest.CreateHttp(url);
-        //    return request;
-        //}
-
         protected abstract Task InitializeViewModelAsync();
 
         public async Task UpdateTile()
