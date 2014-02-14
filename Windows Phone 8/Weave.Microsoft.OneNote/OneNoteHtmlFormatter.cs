ac6d9e1394f4dae0d755047685137d0cdf847573
Arash Emami <aemami99@gmail.com>
2014-02-13 16:05:55 -0800

    improvements to the onenote libraries

--- Windows Phone 8/Weave.Microsoft.OneNote/OneNoteHtmlFormatter.cs
+++ Windows Phone 8/Weave.Microsoft.OneNote/OneNoteHtmlFormatter.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Weave.Microsoft.OneNote
 {
@@ -14,12 +10,21 @@ namespace Weave.Microsoft.OneNote
 "<title>{0}</title>" +
 "<meta name=\"created\" content=\"{1}\" />" +
 "</head>" +
-"<body><h3>{3}</h3></body>" +
+"<body>" +
+"<h3>{2}</h3>" + 
+"<a href=\"{3}\"><img data-render-src=\"{3}\" alt=\"{0}\" height=\"600\" width=\"480\" /></a>" +
+"</body>" +
 "</html>";
 
         public string CreateHtml(HtmlLinkOneNoteItem oneNoteItem)
         {
-            return "";
+            var timestamp = DateTime.UtcNow.ToString();
+
+            return string.Format(TEMPLATE,
+                oneNoteItem.Title,
+                timestamp,
+                oneNoteItem.Source,
+                oneNoteItem.Link);
         }
     }
 }
