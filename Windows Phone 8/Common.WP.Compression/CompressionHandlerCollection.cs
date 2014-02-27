e5ac77bb2d36056dcea6e85bf7f5c0e2507e105e
Arash Emami <aemami99@gmail.com>
2014-02-27 03:38:52 -0800

    added wp8 specific compression stuff

---
+++ Windows Phone 8/Common.WP.Compression/CompressionHandlerCollection.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Common.WP.Compression
+{
+    public class CompressionHandlerCollection : Common.Compression.CompressionHandlerCollection
+    {
+        public CompressionHandlerCollection()
+        {
+            this.Add(new GZipHandler());
+            //this.Add(new DeflateHandler());
+        }
+    }
+}
---
+++ Windows Phone 8/Common.WP.Compression/GZipHandler.cs
@@ -0,0 +1,24 @@
+﻿using Common.Compression;
+using ICSharpCode.SharpZipLib.GZip;
+using System.IO;
+
+namespace Common.WP.Compression
+{
+    public class GZipHandler : CompressionHandler
+    {
+        public GZipHandler()
+        {
+            SupportedEncodings.Add("gzip");
+        }
+
+        public override Stream Compress(Stream inputStream)
+        {
+            return new GZipOutputStream(inputStream);
+        }
+
+        public override Stream Decompress(Stream inputStream)
+        {
+            return new GZipInputStream(inputStream);
+        }
+    }
+}
