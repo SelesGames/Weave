d768f2bd5e3d310c916ecfcc6a77b57f116a14cc
Arash Emami <aemami99@gmail.com>
2014-02-27 18:06:27 -0800

    fixed FeedSearchService.cs

--- Windows Phone 8/Weave.FeedSearchService/FeedSearchService.cs
+++ Windows Phone 8/Weave.FeedSearchService/FeedSearchService.cs
@@ -1,4 +1,4 @@
-﻿using SelesGames.Rest;
+﻿using SelesGames.HttpClient;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -11,38 +11,6 @@ namespace Weave.FeedSearchService
 {
     public class FeedSearchService
     {
-        RestClient googleSearchClient, directSearchClient;
-
-        public FeedSearchService()
-        {
-            googleSearchClient = new SelesGames.Rest.JsonDotNet.JsonDotNetRestClient();
-            directSearchClient = new DelegateRestClient(stream =>
-            {
-                using (var reader = XmlReader.Create(stream))
-                {
-                    var feed = SyndicationFeed.Load(reader);
-                    var result = new FeedApiResult
-                    {
-                        responseStatus = "200",
-                        responseData = new ResponseData
-                        {
-                            entries = new List<Entry> 
-                            { 
-                                new Entry 
-                                { 
-                                    //url = feedUrl, 
-                                    title = feed.Title.Text, 
-                                    contentSnippet = feed.Description.Text,
-                                }
-                            }
-                        }
-                    };
-                    reader.Close();
-                    return result;
-                }
-            });
-        }
-
         public async Task<FeedApiResult> SearchForFeedsMatching(string searchString, CancellationToken cancelToken)
         {
             FeedApiResult result;
@@ -61,13 +29,43 @@ namespace Weave.FeedSearchService
             return result;
         }
 
+
+
+
+        #region Private helper methods that either open a feed directly or search based on input
+
         // Call the RSS url directly, extract it's name and description
         async Task<FeedApiResult> DirectSearchForFeed(string feedUrl, CancellationToken cancelToken)
         {
-            var result = await directSearchClient.GetAsync<FeedApiResult>(feedUrl, cancelToken);
-            foreach (var entry in result.responseData.entries)
-                entry.url = feedUrl;
-            return result;
+            var client = new SmartHttpClient();
+            var response = await client.GetAsync(feedUrl, cancelToken);
+
+            using (var responseStream = await client.GetStreamAsync(feedUrl))
+            using (var reader = XmlReader.Create(responseStream))
+            {
+                var feed = SyndicationFeed.Load(reader);
+                var result = new FeedApiResult
+                {
+                    responseStatus = "200",
+                    responseData = new ResponseData
+                    {
+                        entries = 
+                            new List<Entry> 
+                            { 
+                                new Entry 
+                                { 
+                                    //url = feedUrl, 
+                                    title = feed.Title.Text, 
+                                    contentSnippet = feed.Description.Text,
+                                }
+                            }
+                    }
+                };
+                reader.Close();
+                foreach (var entry in result.responseData.entries)
+                    entry.url = feedUrl;
+                return result;
+            }
         }
 
         // Search using Google's RSS search service
@@ -77,8 +75,11 @@ namespace Weave.FeedSearchService
                 "http://ajax.googleapis.com/ajax/services/feed/find?q={0}&v=1.0",
                 HttpUtility.UrlEncode(searchString));
 
-            var result = await googleSearchClient.GetAsync<FeedApiResult>(url, cancelToken);
+            var client = new SmartHttpClient();
+            var result = await client.GetAsync<FeedApiResult>(url, cancelToken);
             return result;
         }
+
+        #endregion
     }
 }
