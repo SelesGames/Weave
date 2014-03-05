c3780b3de0a7a2aa3713441bb36042b4f57ca9fc
Arash Emami <aemami99@gmail.com>
2014-03-04 23:01:02 -0800

    added improved logic via Workflow classes for OneNote and Pocket saving

--- Windows Phone 8/Weave/Services/OneNote/OneNoteHelper.cs
+++ Windows Phone 8/Weave/Services/OneNote/OneNoteHelper.cs
@@ -1,9 +1,5 @@
-﻿using Common.Microsoft.OneNote.Response;
-using SelesGames;
-using System;
+﻿using SelesGames;
 using System.Threading.Tasks;
-using System.Windows;
-using Weave.Microsoft.OneNote;
 using Weave.SavedState;
 using Weave.ViewModels;
 using Weave.ViewModels.Mobilizer;
@@ -12,126 +8,14 @@ namespace Weave.Services.OneNote
 {
     public class OneNoteHelper
     {
-        const string CALLBACK_URI = "http://www.selesgames.com";
-
-        PermanentState permState;
-        MobilizedArticle article;
-        NewsItem newsItem;
-        bool isArticleMobilizedSuccessfully;
-
         readonly static OneNoteHelper current = new OneNoteHelper();
         public static OneNoteHelper Current { get { return current; } }
 
-        public async Task Save(MobilizedArticle article, NewsItem newsItem, bool isArticleMobilizedSuccessfully)
+        public Task Save(MobilizedArticle article, NewsItem newsItem, bool isArticleMobilizedSuccessfully)
         {
-            if (article == null || newsItem == null)
-                return;
-
-            this.article = article;
-            this.newsItem = newsItem;
-            this.isArticleMobilizedSuccessfully = isArticleMobilizedSuccessfully;
-
-            await TrySave(continueOnSaveFailure: true);
-        }
-
-
-        async Task TrySave(bool continueOnSaveFailure)
-        {
-            try
-            {
-                if (continueOnSaveFailure)
-                {
-                    await TrySaveRegisterOnFail();
-                }
-                else
-                {
-                    await InnerSave();
-                }
-            }
-            catch (Exception ex)
-            {
-                DebugEx.WriteLine(ex);
-                MessageBox.Show("Unable to save to OneNote");
-            }
-        }
-
-        async Task TrySaveRegisterOnFail()
-        {
-            try
-            {
-                await InnerSave();
-                return;
-            }
-            catch (Exception ex)
-            {
-                DebugEx.WriteLine(ex);
-            }
-
-            await Register();
-        }
-
-
-        async Task InnerSave()
-        {
-            if (article == null || newsItem == null)
-                return;
-
-            permState = ServiceResolver.Get<PermanentState>();
-            var accessToken = permState.ThirdParty.LiveAccessToken;
-
-            if (accessToken == null)
-            {
-                throw new Exception("accessToken not set");
-            }
-
-            Func<Task<BaseResponse>> saveTask;
-
-            if (isArticleMobilizedSuccessfully)
-            {
-                var oneNoteSave = new MobilizedOneNoteItem
-                {
-                    Title = article.Title,
-                    Link = article.Link,
-                    Source = article.CombinedPublicationAndDate,
-                    HeroImage = article.HeroImageUrl,
-                    BodyHtml = article.ContentHtml,
-                };
-                saveTask = () => oneNoteSave.SendToOneNote(accessToken);
-            }
-
-            else
-            {
-                var oneNoteSave = new HtmlLinkOneNoteItem
-                {
-                    Title = newsItem.Title,
-                    Link = newsItem.Link,
-                    Source = newsItem.FormattedForMainPageSourceAndDate,
-                };
-                saveTask = () => oneNoteSave.SendToOneNote(accessToken);
-            }
-
-            await GlobalDispatcher.Current.InvokeAsync(() => ToastService.ToastPrompt("Sending to OneNote..."));
-
-            var response = await saveTask();
-            if (response is CreateSuccessResponse)
-            {
-                await GlobalDispatcher.Current.InvokeAsync(() => ToastService.ToastPrompt("Saved to OneNote!"));
-            }
-            else
-            {
-                await GlobalDispatcher.Current.InvokeAsync(() => ToastService.ToastPrompt("ERROR saving to OneNote"));
-            }
-        }
-
-        async Task Register()
-        {
-            await GlobalDispatcher.Current.InvokeAsync(() => 
-                GlobalNavigationService.ToOneNoteSignInPage(async () => await OnCallback()));
-        }
-
-        async Task OnCallback()
-        {
-            await TrySave(continueOnSaveFailure: false);
+            var permState = ServiceResolver.Get<PermanentState>();
+            var workflow = new OneNoteSavingWorkflow(permState, article, newsItem, isArticleMobilizedSuccessfully);
+            return workflow.Save();
         }
     }
 }
--- Windows Phone 8/Weave/Services/Pocket/PocketHelper.cs
+++ Windows Phone 8/Weave/Services/Pocket/PocketHelper.cs
@@ -1,8 +1,5 @@
-﻿using PocketSharp;
-using SelesGames;
-using System;
+﻿using SelesGames;
 using System.Threading.Tasks;
-using System.Windows;
 using Weave.SavedState;
 using Weave.Settings;
 using Weave.ViewModels;
@@ -11,135 +8,15 @@ namespace Weave.Services.Pocket
 {
     public class PocketHelper
     {
-        const string CALLBACK_URI = "http://www.selesgames.com";
-
-        readonly string consumerKey;
-
-        PermanentState permState;
-        NewsItem currentNewsItem;
-
         readonly static PocketHelper current = new PocketHelper();
         public static PocketHelper Current { get { return current; } }
 
-        public PocketHelper()
-        {
-            this.consumerKey = AppSettings.Instance.ThirdParty.Pocket.ConsumerKey;
-        }
-
         public Task Save(NewsItem newsItem)
         {
-            return TrySave(newsItem, continueOnSaveFailure: true);
-        }
-
-        async Task TrySave(NewsItem newsItem, bool continueOnSaveFailure)
-        {
-            if (newsItem == null)
-                return;
-
-            currentNewsItem = newsItem;
-
-            try
-            {
-                if (continueOnSaveFailure)
-                {
-                    await TrySaveRegisterOnFail(newsItem);
-                }
-                else
-                {
-                    await InnerSave(newsItem);
-                }
-            }
-            catch(Exception ex)
-            {
-                DebugEx.WriteLine(ex);
-                MessageBox.Show("Unable to save to Pocket");
-            }
-        }
-
-        async Task TrySaveRegisterOnFail(NewsItem newsItem)
-        {
-            try
-            {
-                await InnerSave(newsItem);
-                return;
-            }
-            catch(Exception ex)
-            {
-                DebugEx.WriteLine(ex);
-            }
-
-            await Register();
-        }
-
-        async Task InnerSave(NewsItem newsItem)
-        {
-            permState = ServiceResolver.Get<PermanentState>();
-            var accessCode = permState.ThirdParty.PocketAccessCode;
-
-            if (string.IsNullOrWhiteSpace(accessCode))
-            {
-                throw new Exception("access code not set");
-            }
-
-            var client = new PocketClient(
-                consumerKey: consumerKey,
-                accessCode: accessCode,
-                isMobileClient: true);
-
-            try
-            {
-                ToastService.ToastPrompt("Sending to Pocket...");
-
-                var result = await client.Add(
-                    uri: new Uri(newsItem.Link),
-                    title: newsItem.Title,
-                    tags: new[] { newsItem.Feed.Category, newsItem.Feed.Name });
-
-                ToastService.ToastPrompt("Saved to Pocket!");
-            }
-            catch(Exception ex)
-            {
-                DebugEx.WriteLine(ex);
-                throw;
-            }
-        }
-
-        async Task Register()
-        {
-            var client = new PocketClient(
-                consumerKey: consumerKey,
-                callbackUri: CALLBACK_URI,
-                isMobileClient: true);
-
-            var requestCode = await client.GetRequestCode();
-            var authenticationUri = client.GenerateAuthenticationUri(requestCode);
-
-            await GlobalDispatcher.Current.InvokeAsync(() =>
-                GlobalNavigationService.ToOAuthPage(
-                    authenticationUri.OriginalString,
-                    async () => await OnCallback(requestCode)));
-        }
-
-        async Task OnCallback(string requestCode)
-        {
-            bool isRegSuccessful = false;
-
-            var client = new PocketClient(consumerKey: consumerKey);
-
-            try
-            {
-                var response = await client.GetUser(requestCode: requestCode);
-                permState.ThirdParty.PocketAccessCode = response.Code;
-                isRegSuccessful = true;
-            }
-            catch(Exception ex)
-            {
-                DebugEx.WriteLine(ex);
-                isRegSuccessful = false;
-            }
-
-            if (isRegSuccessful && currentNewsItem != null)
-                await TrySave(currentNewsItem, continueOnSaveFailure: false);
+            var consumerKey = AppSettings.Instance.ThirdParty.Pocket.ConsumerKey;
+            var permState = ServiceResolver.Get<PermanentState>();
+            var workflow = new PocketSavingWorkflow(consumerKey, permState, newsItem);
+            return workflow.Save();
         }
     }
 }
